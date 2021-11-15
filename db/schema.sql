2d1117ecac19ac296a25c52491e11e0846abd080
unknown <jessedraper2@gmail.com>
2021-11-15 14:12:39 -0700

    add inquirer prompts and callback functions

--- db/schema.sql
+++ db/schema.sql
@@ -2,19 +2,25 @@ DROP DATABASE IF EXISTS ems_db;
 CREATE DATABASE ems_db;
 USE DATABASE ems_db;
 CREATE TABLE Department(
-    id: INT PRIMARY KEY,
-    name: VARCHAR(30)
+    id: INT,
+    names: VARCHAR(30),
+    PRIMARY KEY(id)
 );
 CREATE TABLE roles(
-    id: INT PRIMARY KEY,
+    id: INT ,
     title: VARCHAR(30) NOT NULL,
     salary:DECIMAL NOT NULL,
-    department_id: INT NOT NULL
+    FOREIGN KEY(Department_id)
+    REFERENCES Departments(id)
+    ON DELETE SET NULL,
+    PRIMARY KEY(id)
 );
 CREATE TABLE employee(
     id: INT PRIMARY KEY,
     first_name: VARCHAR(30) NOT NULL,
     last_name: VARCHAR(30) NOT NULL,
-    role_id:INT,
+    FOREIGN KEY(roles_id)
+    REFERENCES roles(id)
+    ON DELETE SET NULL,
     manager_id:INT
 )
--- server.js
+++ server.js
@@ -1,16 +1,42 @@
+//packages
 const mysql = require('mysql2');
 const inquirer= require('inquirer');
 const rowGen = require("console.table");
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
+//mysql connection
 const connection= mysql.createConnection({
     host:'localhost',
     user:"root",
     password:"root",
     database:"ems_db"
 
-})
+},
+console.log("Connected to Database..."));
 
-inquirer.list([{
-    name:"firstResp",
+function promptCMD (){ inquirer.prompt([{
+    type: 'list',
+    name:"answer1",
     message: "Please Select a following option:",
     choices: ["View all departments","View all roles", "View all employees", "Add a department","Update employee role"]
 }])
+.then((answers) => {
+if(answers.choices=choices[0]){
+    connection.query(`SELECT Department`)
+    return promptCMD();
+}
+else if(answers.choices=choices[1]){
+    connection.query(`SELECT roles`)
+    return promptCMD();
+}
+else if(answers.choices=choices[2]){
+    connection.query(`SELECT employee`)
+    return promptCMD();
+}
+else if(answers.choices=choices[3]){
+    connection.query(`SELECT roles`)
+}
+})};
+function addDepartment(){};
+function updateRole(){};
