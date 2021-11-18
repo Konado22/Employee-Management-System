3d22e21417e9d54ad14178644b07a2052a7a952b
unknown <jessedraper2@gmail.com>
2021-11-17 17:11:48 -0700

    create functions and callbacks

--- server.js
+++ server.js
@@ -1,42 +1,90 @@
 //packages
-const mysql = require('mysql2');
-const inquirer= require('inquirer');
+const mysql = require("mysql2");
+const inquirer = require("inquirer");
 const rowGen = require("console.table");
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
 //mysql connection
-const connection= mysql.createConnection({
-    host:'localhost',
-    user:"root",
-    password:"root",
-    database:"ems_db"
-
-},
-console.log("Connected to Database..."));
-
-function promptCMD (){ inquirer.prompt([{
-    type: 'list',
-    name:"answer1",
-    message: "Please Select a following option:",
-    choices: ["View all departments","View all roles", "View all employees", "Add a department","Update employee role"]
-}])
-.then((answers) => {
-if(answers.choices=choices[0]){
-    connection.query(`SELECT Department`)
-    return promptCMD();
+const connection = mysql.createConnection(
+  {
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "ems_db",
+  },
+  console.log(`Connected to Database at http://localhost:${PORT} `)
+);
+//routes
+app.get("/", promptCMD);
+app.get("/department", promptCMD);
+app.get("/roles", promptCMD);
+app.get("/");
+//route callbacks
+function promptCMD(req, res) {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "answer1",
+        message: "Please Select a following option:",
+        choices: [
+          "View all departments",
+          "View all roles",
+          "View all employees",
+          "Add a department",
+          "Update employee role",
+        ],
+      },
+    ])
+    .then(async (answers) => {
+      if ((answers.choices = choices[0])) {
+        const allDep = await connection.query(`SELECT * FROM Department`);
+        console.log(allDep);
+        return promptCMD();
+      } else if ((answers.choices = choices[1])) {
+        const allDep = await connection.query(`SELECT * FROM roles`);
+        console.log(allDep);
+        return promptCMD();
+      } else if ((answers.choices = choices[2])) {
+        const allDep = await connection.query(`SELECT * FROM employee`);
+        console.log(allDep);
+        return promptCMD();
+      } else if ((answers.choices = choices[3])) {
+        connection.query(`SELECT roles`);
+      }
+    });
 }
-else if(answers.choices=choices[1]){
-    connection.query(`SELECT roles`)
-    return promptCMD();
+function addDepartment() {
+  const newDept = {
+    id: "",
+    name: "",
+  };
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the name of the new department",
+        name: "deptName",
+      },
+      {
+        type: "input",
+        message: "What is the department id?",
+        name: "deptId",
+      },
+    ])
+    .then ( async (answers) => {
+      newDept.name = answers.deptName;
+      newDept.id = answer.deptId;
+      const addDept = await connection.query(`INSERT INTO Department(id,name) VALUES (${newDept.id}, ${newDept.name})`)
+    });
 }
-else if(answers.choices=choices[2]){
-    connection.query(`SELECT employee`)
-    return promptCMD();
+function updateRole() {
+  const newRole = {
+    id: "",
+    first_name: "",
+    last_name: "",
+    manager_id: "",
+    
+  };
 }
-else if(answers.choices=choices[3]){
-    connection.query(`SELECT roles`)
-}
-})};
-function addDepartment(){};
-function updateRole(){};
